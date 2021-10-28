b51c86d84d33e702cb8832530b9a930eb1ba20d4
Alexander Broekhuis <alexander.broekhuis@luminis.eu>
2021-10-28 14:45:06 +0200

    Updates

--- use/src/main/java/mypackage/Test.java
+++ use/src/main/java/mypackage/Test.java
@@ -1,17 +1,25 @@
 package mypackage;
 
 import org.apache.felix.dm.annotation.api.Component;
+import org.apache.felix.dm.annotation.api.Inject;
 import org.apache.felix.dm.annotation.api.Start;
+import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
 public class Test {
 
-    private static Logger LOGGER = LoggerFactory.getLogger(Test.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class.getName());
+
+    @Inject
+    private static BundleContext bundleContext;
 
     @Start
     private void start() {
         LOGGER.debug("Some logging for testing!");
+
+        String config = bundleContext.getProperty("logback.configurationFile");
+        System.out.println("config = " + config);
     }
 }
